1f2408abf63cda962469c75847c9bab9cc21ac42
Yash karki <yashkarki61@gmail.com>
2026-05-31 21:24:04 +0530

    LC-destroying-asteroids [Runtime: N/A; Memory: N/A]

--- greedy/destroying-asteroids.cpp
+++ greedy/destroying-asteroids.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        long long currMass = mass;
+
         sort(asteroids.begin(), asteroids.end());
 
         for (int asteroid : asteroids) {
-            if (mass < asteroid){
+            if (currMass < asteroid) {
                 return false;
             }
-            mass += asteroid;
+            currMass += asteroid;
         }
 
         return true;
