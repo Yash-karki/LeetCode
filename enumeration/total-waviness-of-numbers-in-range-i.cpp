d5c29c1b43ad5a53faaa15ba908c54296833521f
Yash karki <yashkarki61@gmail.com>
2026-06-05 00:04:07 +0530

    LC-total-waviness-of-numbers-in-range-i [Runtime: N/A; Memory: N/A]

--- enumeration/total-waviness-of-numbers-in-range-i.cpp
+++ enumeration/total-waviness-of-numbers-in-range-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int totalWaviness(int num1, int num2) {
+    int totalWaviness(int nums1, int nums2) {
         vector<int> vec;
         while(nums1 != 0){
             int a = nums1%10;
