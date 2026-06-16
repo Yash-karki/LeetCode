eef0d55cafc588c155933813393ed08a7197b26b
Yash karki <yashkarki61@gmail.com>
2026-06-16 22:42:39 +0530

    LC-process-string-with-special-operations-i [Runtime: N/A; Memory: N/A]

--- string/process-string-with-special-operations-i.cpp
+++ string/process-string-with-special-operations-i.cpp
@@ -3,14 +3,15 @@ public:
     string processStr(string s) {
         int n = s.size();
         string ans = "";
-        for(int i = 0; i<n; i++){
-            if(s[i] == '#'){
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '#') {
                 ans += ans;
-            }else if(s[i] == '%'){
-                reverse(ans.begin(),ans.end());
-            }else if(s[i] == '*' && (ans.size() != 0)){
-                ans.pop_back();
-            }else{
+            } else if (s[i] == '%') {
+                reverse(ans.begin(), ans.end());
+            } else if (s[i] == '*') {
+                if (!ans.empty())
+                    ans.pop_back();
+            } else {
                 ans += s[i];
             }
         }
