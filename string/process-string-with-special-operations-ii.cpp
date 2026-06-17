74418f7014f974cbca8d501ad440c6e64260304f
Yash karki <yashkarki61@gmail.com>
2026-06-17 22:27:44 +0530

    LC-process-string-with-special-operations-ii [Runtime: N/A; Memory: N/A]

---
+++ string/process-string-with-special-operations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        int n = s.size();
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '#') {
+                ans += ans;
+            } else if (s[i] == '%') {
+                reverse(ans.begin(), ans.end());
+            } else if (s[i] == '*') {
+                if (!ans.empty())
+                    ans.pop_back();
+            } else {
+                ans += s[i];
+            }
+        }
+        if(!ans.empty()){
+            for(int i = 0; i<n; i++){
+                if(i == k){
+                    return ans[i];
+                }
+            }
+        }
+        return '.';
+    }
+};
