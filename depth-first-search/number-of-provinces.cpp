b7b3e033c9dfbb8ae8ca85812ff08c1132ce94a0
Yash karki <yashkarki61@gmail.com>
2026-06-18 23:43:47 +0530

    LC-number-of-provinces [Runtime: N/A; Memory: N/A]

--- depth-first-search/number-of-provinces.cpp
+++ depth-first-search/number-of-provinces.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
 
-    void dfs(int node, vector<int> adjLs[], vector<int> &vis){
-        vis[node] = 1;
-        for(auto it : adjLs[node]){
-            if(vis[it] == 0){
-                dfs(it,adjLs,vis);
+    void bfs(vector<int> &vis, vector<int> adj[], int v){
+        vis[v] = 1;
+        queue<int> q;
+        q.push(v);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it : adj[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
             }
         }
-    }
 
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int v = isConnected.size();
-        vector<int> adjLs[v];
+        vector<int> adj[v];
         for(int i = 0; i<v; i++){
             for(int j = 0; j<v; j++){
                 if(isConnected[i][j] == 1 && i!=j){
-                    adjLs[i].push_back(j);
-                    adjLs[j].push_back(i);
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
         vector<int> vis(v,0);
         int cnt = 0;
         for(int i = 0; i<v; i++){
-            if(vis[i] == 0){
+            if(!vis[i]){
                 cnt++;
-                dfs(i,adjLs,vis);
+                bfs(vis,adj,i);
             }
         }
         return cnt;
