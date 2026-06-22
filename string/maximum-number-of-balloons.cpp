8fb1b09d98466d845ab143a735d3f86b90bb34f7
Yash karki <yashkarki61@gmail.com>
2026-06-22 10:57:00 +0530

    LC-maximum-number-of-balloons [Runtime: N/A; Memory: N/A]

---
+++ string/maximum-number-of-balloons.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int n = text.size();
+        unordered_map<char,int> mp;
+        for(auto it : text){
+            if(it == 'b' || it == 'a' || it == 'l' || it == 'o' || it == 'n'){
+                mp[it]++;
+            }
+        }
+        return min({mp['b'],mp['a'],mp['l']/2,mp['o']/2,mp['n']});
+    }
+};
