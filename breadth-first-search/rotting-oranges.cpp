dae01727e64f547c08ed6a348b5279b7522c7fee
Yash karki <yashkarki61@gmail.com>
2026-06-25 00:04:50 +0530

    LC-rotting-oranges [Runtime: N/A; Memory: N/A]

--- breadth-first-search/rotting-oranges.cpp
+++ breadth-first-search/rotting-oranges.cpp
@@ -3,7 +3,7 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<int,int>,int>> q;
+        queue<pair<pair<int,int>,int>> q;
         int vis[n][m];
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
@@ -22,15 +22,27 @@ public:
         int dcol[] = {0, 1, 0, -1};
         while(!q.empty()){
             int r = q.front().first.first;
-            int c = q.front().first..second;
+            int c = q.front().first.second;
             int t = q.front().second;
             tm = max(tm,t);
             q.pop();
             for(int i = 0; i<4; i++){
                 int nrow = r+drow[i];
                 int ncol = c+dcol[i];
-                if(nrow )
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                    q.push({{nrow,ncol}, t+1});
+                    vis[nrow][ncol] = 2;
+                }
             }
         }
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(vis[i][j] != 2 && grid[i][j] == 1){
+                    return -1;
+                }
+            }
+        }
+        return tm;
     }
 };
