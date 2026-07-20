2eac63e102a7fcb33584e303ccf5e205bf399e39
Yash karki <yashkarki61@gmail.com>
2026-07-20 19:00:47 +0530

    LC-fibonacci-number [Runtime: N/A; Memory: N/A]

--- recursion/fibonacci-number.cpp
+++ recursion/fibonacci-number.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int fib(int n) {
-        vector<int> dp(n+1, -1);
-        
-        if(n == 1 || n == 0){
+
+    int sol(int n, vector<int> &dp){
+        if(n == 0 || n == 1){
             return n;
         }
         if(dp[n] != -1){
             return dp[n];
         }
-        dp[n] = fib(n-1) + fib(n-2);
+        dp[n] = sol(n-1, dp) + sol(n-2, dp);
         return dp[n];
+    }
+
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        return sol(n,dp);
     }
 };
