4fc15b7fb45b1430b87e9e86b67ec71eaea68f6e
Yash karki <yashkarki61@gmail.com>
2026-07-23 00:00:53 +0530

    LC-split-a-string-in-balanced-strings [Runtime: N/A; Memory: N/A]

---
+++ greedy/split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int x = 0;
+        int count = 0;
+        for(int i = 0;i < s.size();i++){
+            if(s[i] == 'L') x++;
+            if(s[i] == 'R') x--;
+            if(x == 0) count++;
+        }
+        return count;
+    }
+};
