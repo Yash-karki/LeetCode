c678c0e65ffdcb3cf3ccacfd8d5f5fbedb160e61
Yash karki <yashkarki61@gmail.com>
2026-07-25 20:23:16 +0530

    LC-sum-of-values-at-indices-with-k-set-bits [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/sum-of-values-at-indices-with-k-set-bits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int countBits(int x){
+        int cnt = 0;
+        while(x){
+            cnt += (x&1);
+            x = x>>1;
+        }
+        return cnt;
+    }
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int sum = 0;
+        int n = nums.size();
+        for(int i = 0; i<n; i++){
+            if(countBits(i) == k){
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+};
