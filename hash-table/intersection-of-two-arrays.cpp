602893431010251b10d07893f8c678dc8b2a8547
Yash karki <yashkarki61@gmail.com>
2026-07-26 22:20:07 +0530

    LC-intersection-of-two-arrays [Runtime: N/A; Memory: N/A]

--- hash-table/intersection-of-two-arrays.cpp
+++ hash-table/intersection-of-two-arrays.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> st;
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        for(int i = 0; i<n1; i++){
-            for(int j = 0; j<n2; j++){
-                if(nums1[i] == nums2[j]){
-                    st.insert(nums1[i]);
-                }
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for(int val:nums1){
+            if(!mp[val]) mp[val]++;
+        }
+        for(int val:nums2){
+            if(mp[val]==1){
+                ans.push_back(val);
+                mp[val]++;
             }
         }
-        return vector<int>(st.begin(), st.end());
+        return ans;
     }
 };
