ea846de189d6640c917d7b3d508322aea47dc093
Yash karki <yashkarki61@gmail.com>
2026-08-01 13:51:44 +0530

    LC-predict-the-winner [Runtime: N/A; Memory: N/A]

--- dynamic-programming/predict-the-winner.cpp
+++ dynamic-programming/predict-the-winner.cpp
@@ -8,7 +8,7 @@ public:
         int left = nums[i] - solve(i+1, j, nums);
         int right = nums[j] - solve(i, j-1, nums);
 
-        return max(lefft, right);
+        return max(left, right);
     }
     bool predictTheWinner(vector<int>& nums) {
         if(solve(0,n, nums) >= 0){
