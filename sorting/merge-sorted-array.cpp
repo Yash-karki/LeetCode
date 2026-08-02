805c8f72dd3725b8b09c2ab6609d2752e2e9d004
Yash karki <yashkarki61@gmail.com>
2026-08-02 19:23:43 +0530

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- sorting/merge-sorted-array.cpp
+++ sorting/merge-sorted-array.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = 0;i<n;i++){
-            nums1[m+i] = nums2[i];
+        // for (int i = 0;i<n;i++){
+        //     nums1[m+i] = nums2[i];
+        // }
+        // sort(nums1.begin(),nums1.end());
+
+        int i = m-1; //ye nums1 ki last value pe point karega
+        int j = n-1; // nums2 ke last me point
+
+        int k = m+n-1;//nums1 ke last me merge krne ke liye
+
+        while( j >= 0){
+            if(i >= 0 && nums1[i] > nums2[i]){
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            }else{
+                nums[k] = nums2[j];
+                k--;
+                j--;
+            }
         }
-        sort(nums1.begin(),nums1.end());
 
     }
 };
