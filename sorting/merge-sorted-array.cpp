50b637f0cab830a2f88a0726d41b8246c385b863
Yash karki <yashkarki61@gmail.com>
2026-08-02 19:24:27 +0530

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- sorting/merge-sorted-array.cpp
+++ sorting/merge-sorted-array.cpp
@@ -12,7 +12,7 @@ public:
         int k = m+n-1;//nums1 ke last me merge krne ke liye
 
         while( j >= 0){
-            if(i >= 0 && nums1[i] > nums2[i]){
+            if(i >= 0 && nums1[i] > nums2[j]){
                 nums1[k] = nums1[i];
                 k--;
                 i--;
