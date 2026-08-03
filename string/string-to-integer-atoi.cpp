c5eed1728812f3747987b5c3c307a91f561347fb
Yash karki <yashkarki61@gmail.com>
2026-08-03 22:04:39 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    void solve(string& s, long long& ans, int i) {
-        if (i >= s.size()) {
-            return;
-        } else if (s[i] < '0' || s[i] > '9') {
-            return;
-        } else {
-            ans = ans * 10 + (s[i] - '0');
-        }
-        solve(s, ans, i + 1);
-    }
-
     int myAtoi(string s) {
-        int n = s.size();
-        int sign = 1;
-        long long ans = 0;
-        int i= 0;
-        while (i < n && s[i] == ' ') {
+        int i=0;
+        int sign=1;
+        long ans=0;
+        while(i<s.length() && s[i]==' ')
+            i++;
+        if(s[i]=='-')
+        {
+            sign=-1;
             i++;
         }
-
-        if (i < n && s[i] == '-') {
-            sign = -1;
+        else if(s[i]=='+')
             i++;
-        } else if (i < n && s[i] == '+') {
-            i++;
+        while(i<s.length())
+        {
+            if(s[i]>='0' && s[i]<='9')
+            {
+                ans=ans*10+(s[i]-'0');
+                if(ans>INT_MAX && sign==-1)
+                    return INT_MIN;
+                else if(ans>INT_MAX && sign==1)
+                    return INT_MAX;
+                i++;
+            }
+            else
+                return ans*sign;
         }
-
-        solve(s, ans, i);
-        return sign*ans;
+        return (ans*sign);
     }
 };
