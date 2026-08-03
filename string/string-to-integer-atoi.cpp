80821ee6283708338fea829b79770f3e3a1a8aa5
Yash karki <yashkarki61@gmail.com>
2026-08-03 21:55:45 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

---
+++ string/string-to-integer-atoi.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void solve(string& s, int& ans, int i) {
+        if (i >= s.size()) {
+            return;
+        } else if (s[i] < '0' || s[i] > '9') {
+            return;
+        } else {
+            ans = ans * 10 + (s[i] - '0');
+        }
+        solve(s, ans, i + 1);
+    }
+
+    int myAtoi(string s) {
+        int n = s.size();
+        int sign = 1;
+        int ans = 0;
+        int i= 0;
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+
+        if (i < n && s[i] == '-') {
+            sign = -1;
+            i++;
+        } else if (i < n && s[i] == '+') {
+            i++;
+        }
+
+        solve(s, ans, i);
+        return sign*ans;
+    }
+};
