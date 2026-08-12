06c7310741cd9f40d24be8c33f0d2ca9d22246da
Yash karki <yashkarki61@gmail.com>
2026-08-12 22:52:43 +0530

    LC-power-of-two [Runtime: N/A; Memory: N/A]

--- math/power-of-two.cpp
+++ math/power-of-two.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0){
+        if(n == 0){
             return false;
         }
-        while(n%2 == 0){
-            n = n/2;
-        }
-        return n == 1;
+        return ((n == 1) || (n%2 == 0) && (isPowerOfTwo(n/2)));
     }
 };
