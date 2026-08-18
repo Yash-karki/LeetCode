dfc2ec01de8a607a06094aafbba23b2302833046
Yash karki <yashkarki61@gmail.com>
2026-08-18 22:38:32 +0530

    LC-search-a-2d-matrix-ii [Runtime: N/A; Memory: N/A]

---
+++ binary-search/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int low = 0;
+        int high = m*n-1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(mat[mid/m][mid%m] == target){
+                return true;
+            }else if(mat[mid/m][mid%m] > target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        
+        return false;
+    
+    }
+};
