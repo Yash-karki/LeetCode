fafd6117dc13713f53684f324356a8757625cf80
Yash karki <yashkarki61@gmail.com>
2026-08-19 23:21:14 +0530

    LC-find-the-smallest-divisor-given-a-threshold [Runtime: N/A; Memory: N/A]

--- array/find-the-smallest-divisor-given-a-threshold.cpp
+++ array/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
-        
+        int maxi = *max_element(nums.begin(), nums.end());
+        int ans;
+        int n = nums.size();
+        for (int i = 1; i <= maxi; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += ceil((double)nums[j]/i);
+            }
+            if(sum<=threshold){
+                ans = i;
+                break;
+            }
+        }
+        return ans;
     }
 };
