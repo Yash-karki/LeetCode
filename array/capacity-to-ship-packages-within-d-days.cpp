1296f5f05aef3818820bb2dbcdbdf9b3969a35b9
Yash karki <yashkarki61@gmail.com>
2026-08-21 00:35:30 +0530

    LC-capacity-to-ship-packages-within-d-days [Runtime: N/A; Memory: N/A]

---
+++ array/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(),weights.end());
+        int high = 0;
+        int n = weights.size();
+        for(int i = 0; i<n; i++){
+            high+=weights[i];
+        }
+        for(int i = low; i<=high; i++){
+            int sum = 0;
+            int currWt = 0;
+            int daysreq = 1;
+            for(int j = 0; j<n; j++){
+
+                if(currWt + weights[j] > i){
+                    daysreq++;
+                    currWt = 0;
+                }
+                currWt += weights[j];
+            }
+            if(daysreq <= days){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
