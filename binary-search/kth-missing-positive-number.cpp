59a0f6a6a622817a8fe75d7d8d941d5dbc465a6a
Yash karki <yashkarki61@gmail.com>
2026-08-21 23:41:48 +0530

    LC-kth-missing-positive-number [Runtime: N/A; Memory: N/A]

--- binary-search/kth-missing-positive-number.cpp
+++ binary-search/kth-missing-positive-number.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
-        int low = 0;
-        int high = n-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            int miss = arr[mid]-(mid+1);
+        int low = 0;// 0 3 4
+        int high = n-1; // 4 3
+        while(low<=high){ 
+            int mid = low+(high-low)/2; // 2 3 4
+            int miss = arr[mid]-(mid+1); // 1 3 6
             if(miss<k){
                 low = mid+1;
             }else{
                 high = mid-1;
             }
         }
-        return k+high+1;
+        return k+high+1; // 5+3+1
     }
 };
