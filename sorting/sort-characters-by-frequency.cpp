3464364ef60566d78e983c345dd2a1a08433feb1
Yash karki <yashkarki61@gmail.com>
2026-08-23 23:18:59 +0530

    LC-sort-characters-by-frequency [Runtime: N/A; Memory: N/A]

---
+++ sorting/sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<int,int> mpp;
+
+        for(auto ch : s){
+            mpp[ch]++;
+        }
+        string ans = "";
+        while(!mp.empty()){
+            char maxChar = 0;
+            char MaxFreq = 0;
+            for(auto it : mpp){
+                if(it.second > maxFreq){
+                    maxFreq = it.second;
+                    maxChar = it.first;
+                }
+            }
+            while(maxFreq--){
+                ans.push_back(maChar);
+            }
+            mpp.erase(maxChar);
+        }
+        return ans;
+        
+    }
+};
