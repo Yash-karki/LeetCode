d0b3ff94a39ee4ae3ed22148e025bc3911834c34
Yash karki <yashkarki61@gmail.com>
2026-08-23 23:22:24 +0530

    LC-sort-characters-by-frequency [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/sort-characters-by-frequency.cpp
+++ heap-priority-queue/sort-characters-by-frequency.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<int,int> mpp;
+        unordered_map<char,int> mpp;
 
         for(auto ch : s){
             mpp[ch]++;
@@ -9,7 +9,7 @@ public:
         string ans = "";
         while(!mpp.empty()){
             char maxChar = 0;
-            char maxFreq = 0;
+            int maxFreq = 0;
             for(auto it : mpp){
                 if(it.second > maxFreq){
                     maxFreq = it.second;
