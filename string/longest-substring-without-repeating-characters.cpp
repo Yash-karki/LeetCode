f67498269483a3337ba304d0667b6c346262658d
Yash karki <yashkarki61@gmail.com>
2026-08-25 23:48:38 +0530

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- string/longest-substring-without-repeating-characters.cpp
+++ string/longest-substring-without-repeating-characters.cpp
@@ -5,7 +5,7 @@ public:
         int l = 0;
         int r = 0;
         int ans = 0;
-        vector<int> hashMap(256,-1);
+        vector<int> hashMap(128,-1);
         while(r<n){
             if(hashMap[s[r]] != -1){
                 l = max(hashMap[s[r]]+1,l);
