f6d5ea9d436e1bfae663ccddef0b2672d84267df
Yash karki <yashkarki61@gmail.com>
2026-08-25 23:43:38 +0530

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- hash-table/longest-substring-without-repeating-characters.cpp
+++ hash-table/longest-substring-without-repeating-characters.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
+        int l = 0;
+        int r = 0;
         int ans = 0;
-        for(int i = 0; i<n; i++){
-            vector<int> arr(256,0);
-            for(int j = i; j<n; j++){
-                if(arr[s[j]] == 1) break;
-                int len = j-i+1;
-                ans = max(ans,len);
-                arr[s[j]]++;
+        vector<int> hashMap(256,-1);
+        while(r<n){
+            if(hashMap[s[r]] != -1){
+                l = max(hashMap[s[r]]+1,1);
             }
+            int len = r-l+1;
+            ans = max(ans,len);
+            hashMap[s[r]] = r;
+            r++;
         }
         return ans;
     }
