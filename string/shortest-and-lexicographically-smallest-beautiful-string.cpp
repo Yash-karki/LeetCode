f563a5cfcccabc627e8786b3f74909696c9c6bab
Yash karki <yashkarki61@gmail.com>
2026-08-26 19:22:06 +0530

    LC-shortest-and-lexicographically-smallest-beautiful-string [Runtime: N/A; Memory: N/A]

---
+++ string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        for (int i = k; i < s.size(); i++) {
+            string ans = "";
+            for (int j = 0; j <= s.size() - i; j++) {
+                string temp = s.substr(j, i);
+                int ones = 0;
+                for (auto ch : temp) {
+                    if (ch == '1') {
+                        ones++;
+                    }
+                }
+                if (ones == k) {
+                    if (ans.empty() || temp > ans) {
+                        ans = temp;
+                    }
+                }
+            }
+            if(!ans.empty()){
+                return ans;
+            }
+        }
+        return "";
+    }
+};
