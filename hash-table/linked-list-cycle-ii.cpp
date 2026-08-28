5e704737e22a73f6c80e4a0721330236be9731e5
Yash karki <yashkarki61@gmail.com>
2026-08-28 22:35:21 +0530

    LC-linked-list-cycle-ii [Runtime: N/A; Memory: N/A]

--- hash-table/linked-list-cycle-ii.cpp
+++ hash-table/linked-list-cycle-ii.cpp
@@ -11,13 +11,11 @@ public:
     ListNode *detectCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
-
         while(fast != nullptr && fast->next != nullptr){
             slow = slow->next;
             fast = fast->next->next;
             if(slow == fast){
                 slow = head;
-
                 while(slow != fast){
                     slow = slow->next;
                     fast = fast->next;
