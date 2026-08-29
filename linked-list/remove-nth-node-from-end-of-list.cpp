e6a1f1d4af7833a7bde2931103c4433cd617c164
Yash karki <yashkarki61@gmail.com>
2026-08-29 23:21:07 +0530

    LC-remove-nth-node-from-end-of-list [Runtime: N/A; Memory: N/A]

--- linked-list/remove-nth-node-from-end-of-list.cpp
+++ linked-list/remove-nth-node-from-end-of-list.cpp
@@ -11,22 +11,17 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // if(head == nullptr || head->next == nullptr){
-        //     return nullptr;
-        // }
-
-        ListNode* slow = head;
         ListNode* fast = head;
+        ListNode* slow = head;
         for(int i = 0; i<n; i++){
             fast = fast->next;
         }
-        if(fast ==  nullptr){
+        if(fast == nullptr){
             return head->next;
         }
         while(fast->next != nullptr){
             slow = slow->next;
             fast = fast->next;
-
         }
         slow->next = slow->next->next;
         return head;
