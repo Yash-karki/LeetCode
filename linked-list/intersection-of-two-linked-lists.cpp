ad68d33c5fedec0afd3e9eea9c6119b7caaf1a64
Yash karki <yashkarki61@gmail.com>
2026-09-01 23:48:41 +0530

    LC-intersection-of-two-linked-lists [Runtime: N/A; Memory: N/A]

--- linked-list/intersection-of-two-linked-lists.cpp
+++ linked-list/intersection-of-two-linked-lists.cpp
@@ -8,21 +8,23 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
         // if(headA == nullptr || headB == nullptr){
         //     return nullptr;
         // }
         ListNode* temp1 = headA;
         ListNode* temp2 = headB;
-        while(temp1 != temp2){
-            if(temp1 == nullptr){
+        while (temp1 != temp2) {
+            if (temp1 == nullptr) {
                 temp1 = headB;
+            } else {
+                temp1 = temp1->next;
             }
-            temp1 = temp1->next;
-            if(temp2 == nullptr){
+            if (temp2 == nullptr) {
                 temp2 = headA;
+            } else {
+                temp2 = temp2->next;
             }
-            temp2 = temp2->next;
         }
         return temp1;
     }
