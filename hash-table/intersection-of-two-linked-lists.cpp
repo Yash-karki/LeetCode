17b4f402a3eca90e2f755a526b8d2542b8a1b44b
Yash karki <yashkarki61@gmail.com>
2026-09-01 23:20:38 +0530

    LC-intersection-of-two-linked-lists [Runtime: N/A; Memory: N/A]

--- hash-table/intersection-of-two-linked-lists.cpp
+++ hash-table/intersection-of-two-linked-lists.cpp
@@ -8,22 +8,38 @@
  */
 class Solution {
 public:
-    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == nullptr || headB == nullptr){
+            return nullptr;
+        }
         ListNode* temp1 = headA;
         ListNode* temp2 = headB;
-
-        while (temp1 != temp2) {
-
-            if (temp1 == nullptr)
-                temp1 = headB;
-            else
+        int sizeA = 0;
+        int sizeB = 0;
+        while(temp1 != nullptr){
+            temp1 = temp1->next;
+            sizeA++;
+        }
+        temp1 = headA;
+        while(temp2 != nullptr){
+            temp2 = temp2->next;
+            sizeB++;
+        }
+        temp2 = headB;
+        if(sizeA > sizeB){
+            while(sizeA != sizeB && temp1 != nullptr){
                 temp1 = temp1->next;
-
-            if (temp2 == nullptr)
-                temp2 = headA;
-            else
+                sizeA--;
+            }
+        }else if(sizeB>sizeA){
+            while(sizeB != sizeA && temp2 != nullptr){
                 temp2 = temp2->next;
+                sizeB--;
+            }
+        }
+        while(temp1 != temp2 && temp1 != nullptr && temp2 != nullptr){
+            temp1 = temp1->next;
+            temp2 = temp2->next;
         }
         return temp1;
     }
