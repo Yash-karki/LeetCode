916d75be622d9fd3b40d77cf4f0a471d7d45e868
Yash karki <yashkarki61@gmail.com>
2026-09-03 00:01:49 +0530

    LC-decode-xored-array [Runtime: N/A; Memory: N/A]

---
+++ array/decode-xored-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> ans;
+        int n = encoded.size();
+        ans.push_back(first);
+        for(int i = 0; i<n; i++){
+            ans.push_back(encoded[i]^ans[i]);
+        }
+        return ans;
+    }
+};
