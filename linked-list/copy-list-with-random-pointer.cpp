68184c0c390ea55de94e2e5730e6c5a8e0644227
Yash karki <yashkarki61@gmail.com>
2026-09-05 00:02:52 +0530

    LC-copy-list-with-random-pointer [Runtime: N/A; Memory: N/A]

--- linked-list/copy-list-with-random-pointer.cpp
+++ linked-list/copy-list-with-random-pointer.cpp
@@ -5,7 +5,7 @@ public:
     int val;
     Node* next;
     Node* random;
-
+    
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -19,29 +19,29 @@ public:
     Node* copyRandomList(Node* head) {
         Node* temp = head;
 
-        while (temp) {
+        while(temp != nullptr){
             Node* copyNode = new Node(temp->val);
-            copyNode->next = temp->next;
+            copyNode-> next = temp->next;
             temp->next = copyNode;
             temp = temp->next->next;
         }
         temp = head;
-        while (temp) {
+        while(temp != nullptr){
             Node* copyNode = temp->next;
-            if (temp->random) {
+            if(temp->random){
                 copyNode->random = temp->random->next;
-            } else {
+            }else{
                 copyNode->random = nullptr;
             }
             temp = temp->next->next;
         }
-        Node* dummy = new Node(-1);
-        Node* res = dummy;
+        Node* dummy = new Node(0);
+        Node* temp1 = dummy;
         temp = head;
-        while (temp) {
-            res->next = temp->next;
+        while(temp!= nullptr){
+            temp1->next = temp->next;
             temp->next = temp->next->next;
-            res = res->next;
+            temp1 = temp1->next;
             temp = temp->next;
         }
         return dummy->next;
