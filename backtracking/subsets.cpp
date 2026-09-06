abf53043cfdc1596d052854166d7ad3963fbccc8
Yash karki <yashkarki61@gmail.com>
2026-09-06 16:26:42 +0530

    LC-subsets [Runtime: N/A; Memory: N/A]

--- backtracking/subsets.cpp
+++ backtracking/subsets.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void solve(int index, vector<int>& nums, vector<int>& current, vector<vector<int>>& result) {
-        result.push_back(current);
-
-        for (int i = index; i < nums.size(); i++) {
-            current.push_back(nums[i]);
-
-            solve(i + 1, nums, current, result);
-
-            current.pop_back();
+    void solve(vector<int>& nums, vector<vector<int>> &res, vector<int>& out, int index) {
+        if(index >= nums.size()){
+            res.push_back(out);
+            return;
         }
+        out.push_back(nums[index]);
+        solve(nums,res,out,index+1);
+        out.pop_back();
+        solve(nums,res,out,index+1);
+
     }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> current;
+        vector<vector<int>> res;
+        vector<int> output;
 
-        solve(0, nums, current, result);
+        solve(nums, res, output, 0);
 
-        return result;
+        return res;
     }
 };
