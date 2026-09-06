8242fa2310326518961d105a1aa79b16a181560c
Yash karki <yashkarki61@gmail.com>
2026-09-06 23:26:24 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
-            i++;
-        if(s[i]=='-')
-        {
-            sign=-1;
+        int i = 0;
+        int sign = 1;
+        int ans = 0;
+        while(i<s.size() && s[i] == ' '){
             i++;
         }
-        else if(s[i]=='+')
+        if(s[i] == '-'){
+            sign = -1;
             i++;
-        while(i<s.length())
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
+        }else if(s[i] == '+'){
+            i++;
+        }
+        while(i<s.size()){
+            if(s[i] >= '0' && s[i] <= '9'){
+                ans*= 10;
+                ans+=s[i] - '0';
                 i++;
             }
-            else
-                return ans*sign;
+            else{
+                break;
+            }
         }
-        return (ans*sign);
+        return ans*sign;
     }
 };
