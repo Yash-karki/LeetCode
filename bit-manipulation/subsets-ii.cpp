564f17b2e97fd0fd808499249c7f96ec60886286
Yash karki <yashkarki61@gmail.com>
2026-09-06 16:32:03 +0530

    LC-subsets-ii [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/subsets-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    void solve(vector<int>& nums, vector<vector<int>>& res, vector<int>& out, int index){
+        if(index >= nums.size()){
+            res.push_back(out);
+            return;
+        }
+        out.push_back(nums[index]);
+        solve(nums,res,out,index+1);
+        out.pop_back();
+        while(index < nums.size()-1 && nums[index]==nums[index+1]){
+            index++;
+        }
+        solve(nums,res,out,index+1);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> output;
+        int index = 0;
+        solve(nums,res,output,index);
+        
+        return res;
+    }
+};
