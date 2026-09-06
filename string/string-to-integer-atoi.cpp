0e3d28b6267136ff9461dd2503e281a35c08c3f9
Yash karki <yashkarki61@gmail.com>
2026-09-06 23:56:02 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -1,33 +1,66 @@
 class Solution {
 public:
+    // int myAtoi(string s) {
+    //     int i = 0;
+    //     int sign = 1;
+    //     long long ans = 0;
+    //     while(i<s.size() && s[i] == ' '){
+    //         i++;
+    //     }
+    //     if(s[i] == '-'){
+    //         sign = -1;
+    //         i++;
+    //     }else if(s[i] == '+'){
+    //         i++;
+    //     }
+    //     while(i<s.size()){
+    //         if(s[i] >= '0' && s[i] <= '9'){
+    //             ans*= 10;
+    //             ans+=s[i] - '0';
+    //             if(ans > INT_MAX && sign == -1){
+    //                 return INT_MIN;
+    //             }else if(ans > INT_MAX && sign == 1){
+    //                 return INT_MAX;
+    //             }
+    //             i++;
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+    //     return ans*sign;
+    // }
+
+    // Recursion
+
+    void solve(int i, long long& ans, string& s) {
+        if (s[i] < '0' || s[i] > '9') {
+            return;
+        }
+        ans *= 10;
+        ans += s[i] - '0';
+        solve(i+1,ans,s);
+    }
+
     int myAtoi(string s) {
         int i = 0;
         int sign = 1;
         long long ans = 0;
-        while(i<s.size() && s[i] == ' '){
+        while (i < s.size() && s[i] == ' ') {
             i++;
         }
-        if(s[i] == '-'){
+        if (s[i] == '-') {
             sign = -1;
             i++;
-        }else if(s[i] == '+'){
+        } else if (s[i] == '+') {
             i++;
         }
-        while(i<s.size()){
-            if(s[i] >= '0' && s[i] <= '9'){
-                ans*= 10;
-                ans+=s[i] - '0';
-                if(ans > INT_MAX && sign == -1){
-                    return INT_MIN;
-                }else if(ans > INT_MAX && sign == 1){
-                    return INT_MAX;
-                }
-                i++;
-            }
-            else{
-                break;
-            }
+        solve(i, ans, s);
+        if (ans > INT_MAX && sign == -1) {
+            return INT_MIN;
+        } else if (ans > INT_MAX && sign == 1) {
+            return INT_MAX;
         }
-        return ans*sign;
+        return ans * sign;
     }
 };
