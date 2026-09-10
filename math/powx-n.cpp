ab4aaa9bb1704e61133ddc0c9956d1907b664577
Yash karki <yashkarki61@gmail.com>
2026-09-10 22:47:48 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

--- math/powx-n.cpp
+++ math/powx-n.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    double solve(double x, int n) {
+        if (n == 1) {
+            return x;
+        }
+        if (n == 0) {
+            return 1.0;
+        }
+        if (n % 2 == 0) {
+            return solve(x * x, n / 2);
+        } else {
+            return x*solve(x, n - 1);
+        }
+    }
+
     double myPow(double x, int n) {
-        return pow(x,n);
+        int num = n;
+        if (num < 0) {
+            return 1.0 / solve(x, -1 * n);
+        }
+        return solve(x, n);
     }
 };
