98089af4b69078a0aa1cff7b3e4de1b81f4d00bb
Yash karki <yashkarki61@gmail.com>
2026-09-13 23:29:56 +0530

    LC-generate-parentheses [Runtime: 3 ms; Memory: 15.8 MB]

--- bracket-sequences/generate-parentheses.cpp
+++ bracket-sequences/generate-parentheses.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
 
-    void solve(string& str,int open, int close, vector<string>& ans){
-        if(open == 0 && close == 0){
-            ans.push_back(str);
+    void solve(string s, int open, int close, int n, vector<string>& ans){
+        if(s.size() == n*2){
+            ans.push_back(s);
             return;
         }
-        if(open == close){
-            string str1 = str;
-            str1.push_back('(');
-            solve(str1,open-1,close,ans);
-        }else if(open == 0){
-            string str1 = str;
-            str1.push_back(')');
-            solve(str1,open,close-1,ans);
-        }else{
-            string str1 = str;
-            string str2 = str;
-            str1.push_back('(');
-            str2.push_back(')');
-            solve(str1,open-1,close,ans);
-            solve(str2,open,close-1,ans);
+        if(open < n){
+            solve(s+'(',open+1,close,n,ans);
+        }
+        if(close < open){
+            solve(s+')',open,close+1,n,ans);
         }
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        string str;
-        int open = n;
-        int close = n;
-        solve(str,open,close,ans);
+        solve("",0,0,n,ans);
         return ans;
     }
 };
