e2268369367a94909feb40971ea754ba679d7037
Yash karki <yashkarki61@gmail.com>
2026-09-18 20:46:39 +0530

    LC-decompress-run-length-encoded-list [Runtime: N/A; Memory: N/A]

--- array/decompress-run-length-encoded-list.cpp
+++ array/decompress-run-length-encoded-list.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     vector<int> decompressRLElist(vector<int>& nums) {
-        vector<int> res;
-        for (int i = 0; i < nums.size(); i += 2)
-            res.insert(res.end(), nums[i], nums[i + 1]);
-        return res;
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i += 2) {
+
+            int freq = nums[i];
+            int val = nums[i + 1];
+
+            for (int j = 0; j < freq; j++) {
+                ans.push_back(val);
+            }
+        }
+
+        return ans;
     }
 };
